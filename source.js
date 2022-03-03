364c1b8e1195181f818d6f9836d84511b3728bfe
No Nevim <cermak.mk@gmail.com>
2022-03-03 08:55:03 +0100

    fixed issue with resetting when squares weren't really removed

--- source.js
+++ source.js
@@ -1,4 +1,5 @@
-let howMany = prompt('How many columns?');
+let howMany = 0;
+
 //let width = '50px'
 //let height = '50px'
 
@@ -7,11 +8,13 @@ const square = document.createElement('div');
 square.classList.add('basicSquare');
 const button = document.querySelector('button');
 square.addEventListener('click', transform);
+button.addEventListener('click', reset);
+
+
+howMany = input();
 
 addSquares(howMany);
-
-
-  
+ 
 
 function addSquares(howMany) {
     container.style = `grid-template-columns: repeat(${howMany}, 1fr); grid-template-rows: repeat(${howMany}, 1fr)`;
@@ -29,13 +32,17 @@ function transform(item, event) {
 }
 
 function reset() {
-    let howMany = prompt('How many columns?');
-    document.querySelectorAll('.basicSquare').forEach(item => {item.classList.remove('transSquare')});
+    howMany = input();
+//    document.querySelectorAll('.basicSquare').forEach( (item) => { item.classList.remove('transSquare')});
+    document.querySelectorAll('.basicSquare').forEach( (item) => { item.parentNode.removeChild(item)});
     addSquares(howMany);
 }
 
+function input() {
+    let columns = Number(prompt('How many columns?'));
+    if ( columns > 200 ) columns = 200;
+    return columns;
+}
 
 
-
-button.addEventListener('click', reset)
 
